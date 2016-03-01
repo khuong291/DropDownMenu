32440b0080272119fb79240d83d5a880bcfe989d
Khuong Pham <dkhuong291@gmail.com>
2016-03-01 16:58:44 +0700

    Add MenuViewController

--- DropDownMenu/MenuViewController.swift
+++ DropDownMenu/MenuViewController.swift
@@ -8,28 +8,59 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var navItem: UIBarButtonItem!
+    @IBOutlet var activeViewContainer: UIView!
+    @IBOutlet var tableViewHeightConstrain: NSLayoutConstraint!
+
+    private var viewControllerArray: [UIViewController] = []
+
+    private var activeViewController: UIViewController? {
+        didSet {
+            removeInActiveViewController(oldValue)
+            updateActiveViewController()
+        }
+    }
+
+    var viewControllers: [UIViewController] {
+
+        get {
+            let immutableCopy = viewControllerArray
+            return immutableCopy
+        }
+
+        set {
+            viewControllerArray = newValue
+
+            if activeViewController == nil || viewControllerArray.indexOf(activeViewController!) == nil {
+                activeViewController = viewControllerArray.first
+            }
+        }
+    }
+
+    private func removeInActiveViewController(inactiveViewController: UIViewController?) {
+        if isViewLoaded() {
+
+        }
+    }
+
+    private func updateActiveViewController() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func didTapMenuButton(sender: AnyObject) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
